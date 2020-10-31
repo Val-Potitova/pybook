8be8336486b9826194ebb054252bd23cccac458f
Val-Potitova <71068874+Val-Potitova@users.noreply.github.com>
2020-10-31 09:53:50 +0300

    Add files via upload

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -35,10 +35,13 @@ def gcd(a: int, b: int) -> int:
     >>> gcd(3, 7)
     1
     """
-    m=1
-    for i in range(2, max(a,b)+1):
-        if a%i==0 and b%i==0:
-            m=i
+    if a==0 and b==0:
+        m=0
+    else:
+        m=1
+        for i in range(2, max(a,b)+1):
+            if a%i==0 and b%i==0:
+                m=i
     return m
     pass
 
