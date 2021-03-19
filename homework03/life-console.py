b79c979ad82f5824ee33236baa91a55e07d6a7d1
Val-Potitova <71068874+Val-Potitova@users.noreply.github.com>
2021-03-19 22:13:28 +0300

    Update life-console.py

--- homework03/life-console.py
+++ homework03/life-console.py
@@ -2,22 +2,39 @@ import curses
 
 from life import GameOfLife
 from ui import UI
+import time
+from time import sleep
 
 
 class Console(UI):
 
     def __init__(self, life: GameOfLife) -> None:
         super().__init__(life)
+        self.screen = curses.initscr()
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        pass
+        self.screen.border(0)
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        pass
+        for y, row in enumerate(self.life.curr_generation):
+            for x, value in enumerate(row):
+                self.sign = "*" if value == 1 else " "
+                self.screen.addch(x + 1, y + 1, self.sign)
 
     def run(self) -> None:
-        screen = curses.initscr()
-        # PUT YOUR CODE HERE
+        self.draw_borders()
+        self.running = True
+        while self.running:
+            self.draw_borders()
+            self.draw_grid()
+            self.life.step()
+            self.screen.refresh()
+            time.sleep(1)
         curses.endwin()
+        
+if __name__ == "__main__":
+    game = GameOfLife(size=(5, 5), randomize=True)
+    console = Console(game)
+    console.run()        
